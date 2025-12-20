1387c2bce14a4685dab892be2e6b064b3450954f
anik999xc <anik999xc@gmail.com>
2025-12-20 16:03:44 +0530

    Fixed .env

--- routes/upload.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadImages } = require('../controllers/uploadController');
-
-router.post('/profile', uploadImages);
-
-module.exports = router;
